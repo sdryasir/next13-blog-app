ec8031dd09d0943551d84675b7a7856ea791479f
Yasir L <yasira@sharepointxperts.com>
2023-01-08 01:50:07 +0500

    important property added in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,11 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  important: true
 };
