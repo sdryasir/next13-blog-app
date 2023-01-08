5d4677ff6c70baf8989b379086d5d386aa87c437
Yasir L <yasira@sharepointxperts.com>
2023-01-09 02:55:05 +0500

    fixed mobile responsive

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -28,7 +28,7 @@ function Navbar() {
               <div>
                 <Link href={`/about`}>
                 <button type="button" className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                  <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                  <img className="h-8 w-8 rounded-full" src="/avatar.jpg" alt="" />
                 </button>
                 </Link>
               </div>
--- app/page.tsx
+++ app/page.tsx
@@ -20,12 +20,12 @@ export default function Home() {
     <>
       <Carousel />
       <main className='container mx-auto'>
-        <h1 className='text-4xl my-6'>Categories</h1>
-        <div className='grid grid-cols-4 gap-4 sm:grid-cols-2 md:grid-cols-4'>
+        <h1 className='text-4xl m-3'>Categories</h1>
+        <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4'>
           {
             productCategories.map((category: string, idx: number) => {
               return (
-                <Link key={idx} href={`/category/${encodeURIComponent(category)}`} className='text-3xl border-2 border-r-amber-100 hover:bg-slate-300 p-5 my-5'>
+                <Link key={idx} href={`/category/${encodeURIComponent(category)}`} className='text-3xl border-2 border-r-amber-100 hover:bg-slate-300 p-5 m-3'>
                   <p>{capitalizeFirstLetter(category)}</p>
                 </Link>
               )
